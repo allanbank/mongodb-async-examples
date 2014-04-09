8e7e0948420400acb02a4f0ff694c0b4653c822d
Robert J. Moore <Robert.J.Moore@allanbank.com>
2014-04-09 00:38:16 -0400

    Update the liscense.

--- src/main/java/aggregation/AggregationLetDemo.java
+++ src/main/java/aggregation/AggregationLetDemo.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *     http: *www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -130,7 +130,7 @@ public class AggregationLetDemo {
                                 "discounted",
                                 cond(field("applyDiscount"), constant(0.9),
                                         constant(1))).in(
-                                                multiply(var("total"), var("discounted")))));
+                                multiply(var("total"), var("discounted")))));
 
         /**
          * <pre>
